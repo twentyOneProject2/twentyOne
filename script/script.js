8add05cc93655fe3168bd5a358daac00429c962b
dlmcivor <dlmcivor@hotmail.com>
2021-03-25 16:07:26 -0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,24 @@
+// start with deck of cards
+    // html button on card back, event handler
+
+// click deck of cards to deal individual cards to 3 players twice
+    // click calls api
+    // return array of 6 random cards
+    // assign 2 cards to each player
+    // get image from array
+
+// add card images to designated empty pockets
+    // assign to image tags in html
+
+// calculate the combined values of the 2 cards for each player
+    // function to get value of cards - property of objects in array
+    // combine the 2 card values
+
+// compare the total values for the players
+    // function to find highest value
+
+// add indication of winner
+    // append html for player with highest score to indicate winner (css animation?)
+
+// player can click reset to clear
+// repeat :)
