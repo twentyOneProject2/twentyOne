14f3c4e3277dce01bb6f49600f8236756f0fc9ae
StephenSchooley <schooley.sa@gmail.com>
2021-03-28 17:04:06 -0400

    fixed event handler

--- script/script.js
+++ script/script.js
@@ -30,7 +30,6 @@ twentyOne.dealCards = () => {
             let p3c1 = cardsArray[4];
             let p3c2 = cardsArray[5];
 
-            // Needs individual div or img tags with in each player div
             // twentyOne.assignImage("ID", p1c1.image, p1c1.value, p1c1.suit);
             // twentyOne.assignImage("ID", p1c2.image, p1c2.value, p1c2.suit);
             // twentyOne.assignImage("ID", p2c1.image, p2c1.value, p2c1.suit);
@@ -43,21 +42,22 @@ twentyOne.dealCards = () => {
             let player3Score = twentyOne.getCardValue(p3c1) + twentyOne.getCardValue(p3c2);
 
             console.log(player1Score);
-            // if (player1Score > player2Score && player1Score > player3Score) {
-            //     // append html
-            // } else if (player2Score > player1Score && player2Score > player3Score) {
-            //     // append html
-            // } else if (player3Score > player1Score && player3Score > player2Score) {
-            //     // append html
-            // } else {
-            //     // append html to tie
-            // }
+
+            if (player1Score > player2Score && player1Score > player3Score) {
+                // append html
+            } else if (player2Score > player1Score && player2Score > player3Score) {
+                // append html
+            } else if (player3Score > player1Score && player3Score > player2Score) {
+                // append html
+            } else {
+                // append html to tie
+            }
 
         })
     };
 
 twentyOne.init = () => {
-    document.querySelector(".cardsToDeal").addEventListener("click", twentyOne.dealCards())
+    document.querySelector(".cardsToDeal").addEventListener("click", twentyOne.dealCards)
 };
 
 twentyOne.init();
